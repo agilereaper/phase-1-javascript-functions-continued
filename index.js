b9f761861dde641fab95fae04e9c610e2d7be4b6
dillon zett <dillonmzett@gmail.com>
2023-11-03 14:32:35 -0500

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(weird = "*") {
+    return function (message = "You are *a hard worker*!"){
+        return message
+    }
+}
