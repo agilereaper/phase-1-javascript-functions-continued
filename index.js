f1f6c13ef5d5eb6e4fc38460e0804223ec874045
dillon zett <dillonmzett@gmail.com>
2023-11-05 12:46:44 -0600

    done

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ saturdayFun("bathe my dog")
 function mondayWork(activity = "go to the office") {
     return `This Monday, I will ${activity}.`
 }
-function wrapAdjective(weird = "*") {
-    return function (message = "You are *a hard worker*!"){
-        return message
+function wrapAdjective(message = "a hard worker") {
+  const innerAdj = function (special = "*") {
+        return `You are ${message}${special}${message}!`;
     }
+    return innerAdj
 }
